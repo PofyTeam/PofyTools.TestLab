44cc130922fa70b1a9bc1dd863f4b9f4a1eebf0a
Studio Guvernal <contact@guvernal.com>
2016-12-31 17:34:28 +0100

    Added music

---
+++ Assets/OpenChestButtonView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PofyTools.UI;
+
+public class OpenChestButtonView : HUDButtonView
+{
+	public Chest chest;
+
+	public override void OnPointerDown (UnityEngine.EventSystems.BaseEventData eventData)
+	{
+		base.OnPointerDown (eventData);
+		this.chest.OnRayHit ();
+	}
+}
---
+++ Assets/PlayMusicOnStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PofyTools.Sound;
+
+public class PlayMusicOnStart : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		SoundManager.PlayMusic ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
