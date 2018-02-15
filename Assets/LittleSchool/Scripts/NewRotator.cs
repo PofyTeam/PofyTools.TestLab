bbc8dec32923dd535c6411ec7fcf7848b2423d26
Ana Stamenković <ana24stamenkovic@gmail.com>
2018-02-15 21:58:51 +0100

    New map tiles

---
+++ Assets/LittleSchool/Scripts/NewRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewRotator: MonoBehaviour  {
+
+    public float rotationSpeed = 4.7f;
+
+    private void Update()
+    {
+        this.transform.Rotate(Vector3.up, this.rotationSpeed * Time.deltaTime);   
+    }
+    
+}
